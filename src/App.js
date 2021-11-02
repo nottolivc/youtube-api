f950b9aeae118fe6021994f5f474d129c0412a87
Andrew S <nottolivc@gmail.com>
2021-11-02 15:21:29 -0700

    init keys for yt api, create hook to consume api in test phase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
+import * as React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+//import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Button } from '@mui/material';
+
+const App = () => {
+  
+const [data, setData] = useState([]);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+const [defaultData, setDefaultData] = useState([])
+//const [keyword, setKeyword] = useState('');
+const baseUrl = `https://www.googleapis.com/youtube/v3`
+  
+
+const fetchData = async (url) => {
+  setLoading(true);
+  const data = await axios.get(`${baseUrl}/${url}`, {
+    params: {
+        //key: 'AIzaSyDVaQm89lEvlCfeB5ArCXPhv12Lak65_nU',
+        key: process.env.REACT_APP_API_KEY,
+        maxResults: '10',
+    },
+  });
+  console.log(data.data);
+  setData(data?.data?.items);
+  setLoading(false);
+};
+
+const fetchDefaultData = async (url) => {
+  const getDefaultData = await axios.get(`${baseUrl}/${url}`, {
+    params: {
+      //key: 'AIzaSyDVaQm89lEvlCfeB5ArCXPhv12Lak65_nU',
+      key: process.env.REACT_APP_API_KEY,
+      maxResults: '10',
+  },
+  });
+  console.log(getDefaultData.data, defaultData);
+  setDefaultData(getDefaultData?.data?.items);
+};
+
+useEffect (() => {
+  setLoading(true);
+  setError(null);
+  try {
+    fetchData('search?part=snippet&type=video&q=react');
+    fetchDefaultData('search?part=snippet&type=video&q=react');
+    } catch (error) {
+      setError(error);
+   }
+  setLoading(false);
+   // eslint-disable-next-line 
+}, []);
+
+return (
+    <>
+      <div className="App">
+      <h1>Youtube API</h1>
+      <br />
+      <input label='Search' />
+      <Button variant="contained">Submit</Button>
+      <br />
+      <br />
+      {loading ? ( <p>Loading...</p> ) : (<p></p>)}
+      {error && <div>Error: {error.message}</div>}
+      
+      {data.map((item) => (
+        <>
+          <div className='video-icon' key={item.snippet.url}>
+            <h2>{item.snippet.title}</h2>
+            <h4>{item.snippet.id}</h4>
+            <img src={item.snippet.thumbnails.medium.url} alt={item.snippet.title} />
+          </div>
+        </>
+        ))}
+      </div>
+    </>
   );
 }
 
